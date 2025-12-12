ade5dd50767cbd90fceb0a4cbe0b0666c2ff5b27
Rudo <rudomusanhu@gmail.com>
2025-12-12 17:51:28 +0200

    istall react dom

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,8 @@
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -13,12 +11,7 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Index />
       </TooltipProvider>
     </QueryClientProvider>
   </ThemeProvider>
